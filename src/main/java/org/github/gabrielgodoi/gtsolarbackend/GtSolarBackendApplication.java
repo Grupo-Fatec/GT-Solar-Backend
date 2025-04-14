a2758548cec917c03c7c18a49ae4800b020840e7
GabrielGodoi452 <gabrielgodoi452@gmail.com>
2025-04-13 23:30:33 -0300

    :tada: initial commit

---
+++ src/main/java/org/github/gabrielgodoi/gtsolarbackend/GtSolarBackendApplication.java
@@ -0,0 +1,13 @@
+package org.github.gabrielgodoi.gtsolarbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtSolarBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GtSolarBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/github/gabrielgodoi/gtsolarbackend/GtSolarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.github.gabrielgodoi.gtsolarbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtSolarBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
