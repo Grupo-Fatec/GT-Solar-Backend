70987db139bf4e9d55f3e282637241bd3e6eb062
gabrielgodoi987 <gabrielgodoi452@gmail.com>
2025-05-24 08:23:23 -0300

    :fire: :fire: auth service successfully done

---
+++ src/main/java/org/github/gabrielgodoi/gtsolarbackend/services/auth/TokenService.java
@@ -0,0 +1,55 @@
+package org.github.gabrielgodoi.gtsolarbackend.services.auth;
+
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import lombok.RequiredArgsConstructor;
+import org.github.gabrielgodoi.gtsolarbackend.entities.Admin;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@RequiredArgsConstructor
+@Service
+public class TokenService {
+    @Value("{api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(Admin admin) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(this.secret);
+            return JWT.create()
+                    .withIssuer("auth")
+                    .withSubject(admin.getEmail())
+                    .withSubject(admin.getName())
+                    .withExpiresAt(this.generateExpirationDate())
+                    .sign(algorithm);
+
+        } catch (JWTCreationException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(this.secret);
+
+            return JWT.require(algorithm)
+                    .withIssuer("auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException e) {
+            return "";
+        }
+    }
+
+    private Instant generateExpirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
---
+++ src/main/java/org/github/gabrielgodoi/gtsolarbackend/services/auth/config/SecurityFilter.java
@@ -0,0 +1,43 @@
+package org.github.gabrielgodoi.gtsolarbackend.services.auth.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.github.gabrielgodoi.gtsolarbackend.repositories.AdminRepository;
+import org.github.gabrielgodoi.gtsolarbackend.services.auth.TokenService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+    private final AdminRepository adminRepository;
+    private final TokenService tokenService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = this.recoverToken(request);
+        if (token != null){
+            String subject = this.tokenService.validateToken(token);
+            UserDetails user = this.adminRepository.findByEmail(subject);
+
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+
+    private String recoverToken(HttpServletRequest request){
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
+    }
+}
