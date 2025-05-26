0d91ad28b9c21a4e84cb9a5d89ed33f1a83e00c7
gabrielgodoi987 <gabrielgodoi452@gmail.com>
2025-05-26 19:03:20 -0300

    :fire:

--- src/main/java/org/github/gabrielgodoi/gtsolarbackend/services/auth/config/SecurityConfiguration.java
+++ src/main/java/org/github/gabrielgodoi/gtsolarbackend/services/auth/config/SecurityConfiguration.java
@@ -3,7 +3,6 @@ package org.github.gabrielgodoi.gtsolarbackend.services.auth.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,7 +15,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RequiredArgsConstructor
